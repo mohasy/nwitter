d267f70688492ee31ae7c89ac2a97c483f20ac8f
mohasy <304182@naver.com>
2021-09-26 11:56:25 +0900

    day1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './App';
+import App from './components/App';
 // import reportWebVitals from './reportWebVitals';
+import fbase from "./fbase";
+// console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
