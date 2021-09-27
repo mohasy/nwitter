9942774beffdce2cc26d438e8962f2f827eada46
mohasy <304182@naver.com>
2021-09-27 15:53:25 +0900

    로그인 기능 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,13 @@
+import { authService } from "fbase";
 import React, {useState} from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
     //login
 const Auth =  () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) => {
       const { target: {name, value} } = event;
@@ -17,9 +21,51 @@ const Auth =  () => {
         }
     };
 
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
         event.preventDefault();  
+        try {
+            let data;
+
+            if(newAccount) {
+                //account 생성
+                data = await createUserWithEmailAndPassword(authService, email, password);
+            }else {
+                //로그인
+                data = await signInWithEmailAndPassword(authService, email, password);
+            }
+
+            console.log(data);
+
+        } catch(error) {
+            setError(error.message);
+        }
     };
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {
+        // console.log(event.target.name);
+        const {
+            target:{name},
+        } = event;
+
+        let provider;
+
+        try{
+            if (name === "google"){
+                provider = new GoogleAuthProvider();
+                const result = await signInWithPopup(authService, provider);
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+            } 
+            else if (name === "github"){
+                provider = new GithubAuthProvider();
+                const result = await signInWithPopup(authService, provider);
+                const credential = GithubAuthProvider.credentialFromResult(result);
+            }
+            // await authService.signInWithPopup(provider);
+
+        }catch(error){
+            console.log(error);
+        }
+    }
 
     return (
         <div>
@@ -27,10 +73,14 @@ const Auth =  () => {
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In" } />
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
             <div>
-                <button>Continue with Google</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with GitHub</button>
+
             </div>
         </div>
     );
