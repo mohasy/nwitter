155b9fce0662fd5614be71d269c8dc4d7c6c6099
mohasy <304182@naver.com>
2021-09-27 16:15:17 +0900

    로그아웃 기능 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,21 +2,33 @@ import React, { useEffect, useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+// import {Redirect} from "react-router";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
                         <Route exact path="/">
                             <Home />
                         </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                        {/* <Redirect from="*" to="/" /> */}
                     </>
                 ) : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route> 
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route> 
+                        {/*  "/" 외의 route로 가면 "/"로 되돌아감 */}
+                        {/* <Redirect from="*" to="/" /> */}
+                    </>
                 )}
             </Switch>
         </Router>
