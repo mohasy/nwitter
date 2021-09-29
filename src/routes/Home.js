11f7f9096339be25a7fc552d3cb3440086623a2d
mohasy <304182@naver.com>
2021-09-29 14:56:37 +0900

    글 작성 & firebase 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,30 @@
-import React from "react";
+import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Home =  () => <span>Home</span>;
+const Home =  () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        await addDoc(collection(dbService, "nweets"),{
+            nweet,
+            createdAt: serverTimestamp(),
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const { target:{value} } = event;
+        setNweet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} type="text" on
+                onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit"
+                value="Nweet" />
+            </form>
+        </div>
+    );
+};
 export default Home;
