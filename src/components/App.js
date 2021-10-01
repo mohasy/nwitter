afaa93ac51bf5a6181c3e0fc5f4550c9e32b761d
mohasy <304182@naver.com>
2021-10-01 17:53:58 +0900

    프로필 수정 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -14,16 +14,29 @@ function App() {
       if(user){
         setIsLoggedIn(true);
         setUserObj(user);
+        // setUserObj({
+        //   displayName: user.displayName,
+        //   uid: user.uid,
+        //   updateProfile: (args) => user.updateProfile(args),
+        // });
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
+
       setInit(true);
     });
   }, []);
 
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({ ...user });
+    setUserObj(user);
+  }
+
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
+      {init ? <AppRouter refreshUser={refreshUser} isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
       <footer>&copy;{new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,64 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React,{useEffect, useState} from "react";
 import { useHistory } from "react-router";
+import { collection, getDocs, query, where } from "@firebase/firestore";
+import { updateProfile } from "@firebase/auth";
 
-export default () => {
+export default ( {refreshUser, userObj} ) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+
+    const onChange = (event) => {
+        const {
+            target: {value}
+        } = event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        if (userObj) {
+            if (userObj.displayName === null) {
+                await userObj.updateProfile({
+                    displayName: "User",
+                });
+            }
+        }
+
+        if(userObj.displayName !== newDisplayName){
+            await updateProfile(await authService.currentUser, {
+                displayName: newDisplayName,
+                });
+        }
+        
+        refreshUser();
+            
+    }
+
+    const getMyNweets = async() => {
+        const q = query(
+            collection(dbService, "nweets"),
+            where("creatorId", "==", userObj.uid)
+        );
+        // const querySnapshot = await getDocs(q);
+        // querySnapshot.forEach((doc) => {
+        //     console.log(doc.id, " => ", doc.data());
+        // });
+    }
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+
     return (
         <>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placeholder="닉네임" />
+            <input type="submit" value="프로필 수정" />
+        </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
